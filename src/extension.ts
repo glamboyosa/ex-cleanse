92447cb184f0283c3c42f758e712705204552b12
Timothy Ogbemudia <ogbemudiatimothy@gmail.com>
2022-06-07 00:59:33 +0100

    feat: build extension core features

---
+++ src/extension.ts
@@ -0,0 +1,118 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from "vscode";
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  const key = "ex-cleanse-keys";
+
+  async function getUserInput() {
+    const input = await vscode.window.showInputBox({
+      prompt: "Enter the name of your logger",
+      placeHolder: "Enter the name of your logger e.g. `IO.puts`",
+    });
+    const value = input;
+
+    return value;
+  }
+  function saveToGlobalState(value: string) {
+    context.globalState.setKeysForSync([key]);
+    const existing: string | undefined = context.globalState.get(key);
+    if (existing) {
+      const existingArray: string[] = JSON.parse(existing);
+      // check if the value is already in the array. If it is display a message telling the user it exists and prompting them to exit
+      if (existingArray.includes(value)) {
+        vscode.window.showErrorMessage(
+          `${value} is already in your logger list.`
+        );
+        return;
+      }
+
+      existingArray.push(value);
+      context.globalState.update(key, JSON.stringify(existingArray));
+    } else {
+      context.globalState.update(key, JSON.stringify([value]));
+    }
+  }
+  function findLogs(editor: vscode.TextEditor) {
+    const text = editor.document.getText();
+    const existing: string | undefined = context.globalState.get(key);
+    if (existing) {
+      const existingArray: string[] = JSON.parse(existing);
+      const endOfRegex = existingArray.join("|");
+      const regex = RegExp(`IO.(puts|inspect)|${endOfRegex}?`, "g");
+      let logs: vscode.Range[] = [];
+      let match;
+      while ((match = regex.exec(text)) !== null) {
+        // This is necessary to avoid infinite loops with zero-width matches
+        if (match.index === regex.lastIndex) {
+          regex.lastIndex++;
+        }
+
+        let range = new vscode.Range(
+          editor.document.positionAt(match.index),
+          editor.document.positionAt(match.index + match[0].length)
+        );
+        logs.push(range);
+      }
+      return logs;
+    } else {
+      const regex = RegExp("IO.(puts|inspect)?", "g");
+      let logs: vscode.Range[] = [];
+      let match;
+      while ((match = regex.exec(text)) !== null) {
+        // This is necessary to avoid infinite loops with zero-width matches
+        if (match.index === regex.lastIndex) {
+          regex.lastIndex++;
+        }
+
+        let range = new vscode.Range(
+          editor.document.positionAt(match.index),
+          editor.document.positionAt(match.index + match[0].length)
+        );
+        logs.push(range);
+      }
+      return logs;
+    }
+  }
+
+  function deleteLogs(logs: vscode.Range[]) {
+    vscode.window.activeTextEditor?.edit((editBuilder) => {
+      logs.forEach((range) => {
+        editBuilder.delete(range);
+      });
+    });
+    vscode.window.showInformationMessage(`Deleted ${logs.length} logs 🚀`);
+  }
+  let disposable = vscode.commands.registerCommand("ex-cleanse.Cleanse", () => {
+    const editor = vscode.window.activeTextEditor;
+
+    if (editor) {
+      const logs = findLogs(editor);
+      deleteLogs(logs);
+    } else {
+      vscode.window.showInformationMessage("No editor open.");
+    }
+  });
+
+  let addLogger = vscode.commands.registerCommand(
+    "ex-cleanse.addLogger",
+    async () => {
+      const input = await getUserInput();
+      input && saveToGlobalState(input);
+    }
+  );
+
+  context.subscriptions.push(disposable);
+  context.subscriptions.push(addLogger);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
